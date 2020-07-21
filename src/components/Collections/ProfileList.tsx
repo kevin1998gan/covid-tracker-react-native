ce842eb49aa425f6fdc150e0fcb22b48e9f051e8
cm-hvg <36766508+cm-hvg@users.noreply.github.com>
2020-07-21 19:07:45 +0100

    feat: profile list animation

--- src/components/Collections/ProfileList.tsx
+++ src/components/Collections/ProfileList.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import React, { useRef, useEffect } from 'react';
+import { StyleSheet, View, Animated, Easing } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import { DrawerNavigationProp } from '@react-navigation/drawer';
 
@@ -43,22 +43,59 @@ export const ProfileList: React.FC<Props> = ({
   if (!isLoaded) {
     return <Loading status={status} error={error} style={{ borderColor: 'green', borderWidth: 1 }} onRetry={onRetry} />;
   }
+  const opacity = { start: 0, end: 1 };
+  const positionY = { start: 75, end: 0 };
+
+  const fadeAnimations = profiles
+    .map((o) => o.id)
+    .concat(['add profile'])
+    .map(() => useRef(new Animated.Value(opacity.start)).current);
+  const animations = profiles
+    .map((o) => o.id)
+    .concat(['add profile'])
+    .map(() => useRef(new Animated.Value(positionY.start)).current);
+
+  useEffect(() => {
+    const run = (fn: any, index: number, final: number) => {
+      const duration = 220;
+      const delay = index * 40 + 150;
+      Animated.timing(fn, {
+        toValue: final,
+        duration,
+        delay,
+        easing: Easing.inOut(Easing.cubic),
+      }).start();
+    };
+    fadeAnimations.forEach((item, index) => run(item, index, opacity.end));
+    animations.forEach((item, index) => run(item, index, positionY.end));
+  }, [fadeAnimations, animations]);
 
   return (
     <View style={styles.profileList}>
       {profiles.map((profile, i) => {
         return (
           <View style={styles.cardContainer} key={profile.id}>
-            <TouchableOpacity onPress={() => onProfileSelected(profile.id, i)}>
-              {renderItem(profile, i)}
-            </TouchableOpacity>
+            <Animated.View
+              style={{
+                opacity: fadeAnimations[i],
+                transform: [{ translateY: animations[i] }],
+              }}>
+              <TouchableOpacity onPress={() => onProfileSelected(profile.id, i)}>
+                {renderItem(profile, i)}
+              </TouchableOpacity>
+            </Animated.View>
           </View>
         );
       })}
-
       {addProfile && (
         <View style={styles.cardContainer} key="new">
-          <TouchableOpacity onPress={addProfile}>{renderCreateItem()}</TouchableOpacity>
+          <Animated.View
+            style={{
+              opacity: fadeAnimations[profiles.length],
+              transform: [{ translateY: animations[profiles.length] }],
+            }}>
+            <TouchableOpacity onPress={addProfile}>{renderCreateItem()}</TouchableOpacity>
+          </Animated.View>
         </View>
       )}
     </View>
